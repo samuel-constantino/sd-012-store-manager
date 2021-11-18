9cefbf33012fb4fd253edc048e03f1e00e3c5cec
Samuel Constantino <samuelicapuidados@gmail.com>
2021-11-18 15:38:07 -0300

    atualiza setup

---
+++ src/models/connection.js
@@ -0,0 +1,6 @@
+const { MongoCliente } = require('mongodb');
+
+require('dotenv').config();
+
+const MONGO_DB_URL = `mongodb://${process.env.HOST || 'mongodb'}:27017/StoreManager`;
+const DB_NAME = 'StoreManager';
